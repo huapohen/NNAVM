6f053ca38169375345dba60fb56e1c9a868027a1
liwenbo <694450321@qq.com>
2023-02-03 19:10:30 +0800

    fev2bev unit test passed

---
+++ git.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+rtdir='/home/data/lwb/code'
+src='dynamicbev'
+dst='NNAVM'
+commit_detail='fev2bev unit test passed'
+
+cd ${rtdir}
+mv ${dst} ${dst}_bp
+cp -r ${rtdir}/${src} ${rtdir}/${dst}
+cd NNAVM
+rm -rf .git README.rd .gitignore
+cd ..
+cp -r ${dst}_bp/.git ${dst}/
+cp ${dst}_bp/README.md ${dst}/
+rm -rf ${dst}_bp
+
+for n in ${src} ${dst}
+do
+    cd ${rtdir}/${n}
+    git add .
+    git status
+    git commit -m "${commit_detail}"
+    git push
+done
+
+cd ${rtdir}/${src}
